562e7d11d02d8cf85e7c0dd9bab6a02c0b2a70b7
deedeecx330 <68820832+deedeecx330@users.noreply.github.com>
2021-04-27 22:03:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,21 +9,3 @@ tmpDir = "/tmp"
 
 # History location
 dbLocation = "{}/.local/skyhook.db".format(os.environ["HOME"])
-
-usage = """
-    skyhook clear history   -   Delete everything from history
-    skyhook list history    -   List all entries in history
-    
-    skyhook search [file name/hash]  -   Search history for entries matching [file name/hash]
-    skyhook delete [file name/hash]  -   Delete entries specified by [file name/hash] from history
-    skyhook save [file name/hash]    -   Save history entries specified by [file name/hash] to the current directory to export.pod
-    skyhook add [name:hash:key]      -   Manually add an entry to history specified by colon-separated values of [name:hash:key]
-    
-    skyhook import [path]   -   Import history from a location specified by [path]
-    skyhook export history  -   Export entire history to the current directory to export.pod
-    
-    skyhook upload [file name]  -   Upload a file specified by [file name] from the current directory to the IPFS network
-    skyhook download [hash]     -   Download a file specified by [hash] from the IPFS network to the current directory
-
-    It is possible to specify multiple values in a form of a comma-separated list for search,delete,save,import,upload,download and add functions.
-"""
