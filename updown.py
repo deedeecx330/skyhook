e5db0f046e8945bf61774dee962e41ab92ee75d3
deedeecx330 <68820832+deedeecx330@users.noreply.github.com>
2021-04-28 09:32:44 +0200

    Update updown.py

--- updown.py
+++ updown.py
@@ -16,25 +16,19 @@ import os, db, aux, config
 peer = ipfsApi.Client(config.host, config.port)
 
 def uploadFile(fileName):
-    
     currentDir = os.getcwd()
-
     if not fileName in [f for f in os.listdir(".") if os.path.isfile(f)]:
         return(1)
     else:
-        
         password = aux.getRandomString(32)
-        
         aesName = "{}.sky".format(fileName)
         tmpPath = "{}/{}".format(config.tmpDir, aesName)
-
         print("[+] Encrypting {}".format(fileName))
         try:
             skyhookfilecrypt.encryptFile(fileName, tmpPath, bytes(password, "ascii"))
         except:
             aux.cleanUp(tmpPath)
             return(2)
-
         os.chdir(config.tmpDir)
         print("[+] Uploading {}".format(fileName))
         try:
@@ -44,41 +38,31 @@ def uploadFile(fileName):
             os.chdir(currentDir)
             aux.cleanUp(tmpPath)
             return(3)
-
         now = datetime.now()
         currentDate = now.strftime("%d/%m/%Y-%H:%M:%S")
         print("[+] Adding entry to history")
-
         res = db.addToHistory(fileName, result["Hash"], password, currentDate)
         if res == 0:
             pass
         else:
             aux.cleanUp(tmpPath)
             return(4)
-
         aux.cleanUp(tmpPath)
-
         return(0)
 
 def downloadFile(fileHash):
-    
     currentDir = os.getcwd()
-
     fileName, password = db.getEntry(fileHash)
-
     if fileName == 1 and password == 1:
         return(1)
-
     saveFile = "{}/{}".format(currentDir, fileName)
     os.chdir(config.tmpDir)
     print("[+] Downloading {}".format(fileName))
-
     try:
         peer.get(fileHash)
     except:
         os.chdir(currentDir)
         return(2)
-
     print("[+] Decrypting {}".format(fileName))
     try:
         skyhookfilecrypt.decryptFile(fileHash, saveFile, bytes(password, "ascii"))
@@ -86,8 +70,6 @@ def downloadFile(fileHash):
         aux.cleanUp(fileHash)
         os.chdir(currentDir)
         return(3)
-
     aux.cleanUp(fileHash)
     os.chdir(currentDir)
-
     return(fileName)
