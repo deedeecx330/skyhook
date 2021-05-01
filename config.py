57fb8cea2306a512cd94da795c9fc4a2d06d75a8
deedeecx330 <68820832+deedeecx330@users.noreply.github.com>
2021-05-01 12:41:55 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
 
-# Using infura as to not require the user to run an IPFS node
-host = "https://ipfs.infura.io"
-port = 5001
+# IPFS node host
+# Example:
+# host = "https://ipfs.infura.io"
+
+# IPFS node port
+# Example:
+# port = 5001
 
 # Temporary directory where all the processing happens
 tmpDir = "/tmp"
