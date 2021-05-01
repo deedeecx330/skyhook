bafa994df42c0e04a371df780afa9a73afb7ad5d
deedeecx330 <68820832+deedeecx330@users.noreply.github.com>
2021-05-01 12:23:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,11 @@ port = 5001
 # Temporary directory where all the processing happens
 tmpDir = "/tmp"
 
+# Skyhook directory
+skyhookDir = "{}/.local".format(os.environ["HOME"])
+
 # History location
-dbLocation = "{}/.local/skyhook.db".format(os.environ["HOME"])
+dbLocation = "{}/skyhook.db".format(skyhookDir)
+
+# Configuration file location
+dbLocation = "{}/skyhook.config".format(skyhookDir)
