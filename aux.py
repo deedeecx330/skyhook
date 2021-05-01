bf8bb8d4656a01890d59ce9189285e80354efeb6
deedeecx330 <68820832+deedeecx330@users.noreply.github.com>
2021-05-01 12:18:53 +0200

    Delete aux.py

--- aux.py
+++
@@ -1,7 +0,0 @@
-import os, random, string
-
-def cleanUp(filePath):
-    os.remove(filePath)
-
-def getRandomString(length):
-    return("".join(random.choice(string.ascii_letters + string.digits) for i in range(length)))
