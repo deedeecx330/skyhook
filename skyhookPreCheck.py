d4708467ee7a789de6348a38950c8d3eecec5212
deedeecx330 <68820832+deedeecx330@users.noreply.github.com>
2021-05-02 13:43:35 +0200

    Update skyhookPreCheck.py

--- skyhookPreCheck.py
+++ skyhookPreCheck.py
@@ -1,32 +1,5 @@
 import skyhookConfig, os
 
-if not os.path.isabs(skyhookConfig.skyhookDir):
-    print("[!] Skyhook directory {} is not on an absolute path".format(skyhookConfig.skyhookDir))
-    exit()
-
-if not os.path.isdir(skyhookConfig.skyhookDir):
-    try:
-        os.makedirs(skyhookConfig.skyhookDir, exist_ok=True)
-    except:
-        print("[!] Could not create Skyhook directory {}".format(skyhookConfig.skyhookDir))
-        exit()
-        
-if not (os.access(skyhookConfig.skyhookDir, os.W_OK) and os.access(skyhookConfig.skyhookDir, os.R_OK)):
-    print("[!] Cannot read or write to and from {}".format(skyhookConfig.skyhookDir))
-    exit()
-        
-if not os.path.isdir(skyhookConfig.tmpDir):
-    print("[!] Temporary directory {} is not a valid directory".format(skyhookConfig.tmpDir))
-    exit()
-
-if not os.path.isabs(skyhookConfig.tmpDir):
-    print("[!] Temporary directory {} is not on an absolute path".format(skyhookConfig.tmpDir))
-    exit()
-
-if not (os.access(skyhookConfig.tmpDir, os.W_OK) and os.access(skyhookConfig.tmpDir, os.R_OK)):
-    print("[!] Cannot read or write to and from {}".format(skyhookConfig.tmpDir))
-    exit()
-
 if not hasattr(skyhookConfig, 'host'):
     print("[!] IPFS Host is not defined\nPlease modify {}".format(skyhookConfig.__file__))
     exit()
@@ -61,4 +34,31 @@ if not hasattr(skyhookConfig, 'skyhookDir'):
     
 if not isinstance(skyhookConfig.skyhookDir, str):
     print("[!] Skyhook directory variable is not a string\nPlease modify {}".format(skyhookConfig.__file__))
+    exit()
+
+if not os.path.isabs(skyhookConfig.skyhookDir):
+    print("[!] Skyhook directory {} is not on an absolute path".format(skyhookConfig.skyhookDir))
+    exit()
+
+if not os.path.isdir(skyhookConfig.skyhookDir):
+    try:
+        os.makedirs(skyhookConfig.skyhookDir, exist_ok=True)
+    except:
+        print("[!] Could not create Skyhook directory {}".format(skyhookConfig.skyhookDir))
+        exit()
+        
+if not (os.access(skyhookConfig.skyhookDir, os.W_OK) and os.access(skyhookConfig.skyhookDir, os.R_OK)):
+    print("[!] Cannot read or write to and from {}".format(skyhookConfig.skyhookDir))
+    exit()
+        
+if not os.path.isdir(skyhookConfig.tmpDir):
+    print("[!] Temporary directory {} is not a valid directory".format(skyhookConfig.tmpDir))
+    exit()
+
+if not os.path.isabs(skyhookConfig.tmpDir):
+    print("[!] Temporary directory {} is not on an absolute path".format(skyhookConfig.tmpDir))
+    exit()
+
+if not (os.access(skyhookConfig.tmpDir, os.W_OK) and os.access(skyhookConfig.tmpDir, os.R_OK)):
+    print("[!] Cannot read or write to and from {}".format(skyhookConfig.tmpDir))
     exit()
